2a11b10e1e87ccc9c3674c237f9b80279e4dfb61
Tammy Ho <tammyhho98@gmail.com>
2021-05-12 22:25:14 +0000

    Removed global variables from script

--- homepage/script.js
+++ homepage/script.js
@@ -1,18 +1,22 @@
 "use strict";
 
 // Highlight Active Nav Link
+const highlightActiveNav = () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+    
+    for (const link of navLinks) {
+        link.addEventListener('click', e => {
+           // Remove active class on current link
+           document.querySelector('.active').classList.remove('active');
+           
+           // Add active class for clicked link
+           this.classList.add('active');
+        });
+    } 
+};
 
-const navLinks = document.querySelectorAll('.nav-link');
 
-for (let link of navLinks) {
-    link.addEventListener('click', e => {
-       // Remove active class on current link
-       document.querySelector('.active').classList.remove('active');
-       
-       // Add active class for clicked link
-       this.classList.add('active');
-    });
-}
+highlightActiveNav();
 
 
 
